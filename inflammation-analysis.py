6ece7b17e7ee50fbb38875d2f26da8f9dc01f00d
rachel3834 <rstreet@lco.global>
2022-07-11 07:27:30 -0700

    Fixed stylistic errors to conform to PEP8

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -13,17 +13,21 @@ def main(args):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    InFiles = args.infiles
-    if not isinstance(InFiles, list):
-        InFiles = [args.infiles]
+    in_files = args.infiles
+    if not isinstance(in_files, list):
+        in_files = [args.infiles]
 
-
-    for filename in InFiles:
+    for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {
+                    'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)
+                    }
 
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -33,7 +37,7 @@ if __name__ == "__main__":
         'infiles',
         nargs='+',
         help='Input CSV(s) containing inflammation series for each patient')
-
+    
     args = parser.parse_args()
 
     main(args)
